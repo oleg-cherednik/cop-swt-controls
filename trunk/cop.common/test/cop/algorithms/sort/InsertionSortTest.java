90ca11b4c6be534b4648f236e715ff8486027b6f
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-08-15 20:19:35 +0000

    Add some tests cases

--- trunk/cop.common/test/cop/algorithms/sort/InsertionSortTest.java
+++ trunk/cop.common/test/cop/algorithms/sort/InsertionSortTest.java
@@ -1,115 +1,68 @@
+/**
+ * <b>License</b>: <a href="http://www.gnu.org/licenses/lgpl.html">GNU Leser General Public License</a>
+ * <b>Copyright</b>: <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
+ * 
+ * $Id:$
+ * $HeadURL:$
+ */
 package cop.algorithms.sort;
 
-import static cop.common.extensions.CollectionExtension.convertToIntArray;
+import static cop.algorithms.sort.InsertionSort.insertionSort;
+import static cop.algorithms.sort.SortTests.sortedIntArray;
+import static cop.algorithms.sort.SortTests.unsortedIntArray;
+import static cop.common.extensions.CollectionExtension.convertToIntegerArray;
+import static cop.common.extensions.ComparatorExtension.createComparator;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author <a href="mailto:abba-bestl@mail.ru">Cherednik, Oleg</a>
+ */
 public class InsertionSortTest
 {
-	private static final String FILENAME = "numbers.txt";
-	private static final String FILEPATH = "test/resources";
-	private static final int READ_ALL = -1;
-	private static final int TOTAL = READ_ALL;
-
-	private int[] unsortedArray;
-	private int[] sortedArray;
+	private int[] arr;
+	private Integer[] arr1;
+	private Comparator<Integer> cmp = createComparator(Integer.class);
 
 	@Before
 	public void setUp() throws Exception
 	{
-		unsortedArray = readDataFile();
+		assertNotNull(unsortedIntArray);
+		assertNotNull(sortedIntArray);
+		assertEquals(sortedIntArray.length, unsortedIntArray.length);
 
-		assertNotNull(unsortedArray);
-		assertTrue(unsortedArray.length > 0);
+		arr = Arrays.copyOf(unsortedIntArray, unsortedIntArray.length);
+		arr1 = convertToIntegerArray(arr);
 
-		sortedArray = sortArray(unsortedArray);
-
-		assertNotNull(sortedArray);
-		assertTrue(sortedArray.length == unsortedArray.length);
-	}
-
-	protected static int[] readDataFile() throws Exception
-	{
-		List<Integer> arr = new ArrayList<Integer>();
-		BufferedReader in = null;
-		FileReader fr = null;
-
-		try
-		{
-			fr = new FileReader(FILEPATH + "/" + FILENAME);
-			in = new BufferedReader(fr);
-			String str = in.readLine();
-			int i = 0;
-
-			while(str != null || (i > 0 && i++ < TOTAL))
-			{
-				arr.add(Integer.parseInt(str));
-				str = in.readLine();
-			}
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			if(in != null)
-			{
-				try
-				{
-					in.close();
-				}
-				catch(IOException e)
-				{}
-
-				in = null;
-			}
-
-			if(fr != null)
-			{
-				try
-				{
-					fr.close();
-				}
-				catch(IOException e)
-				{}
-
-				fr = null;
-			}
-		}
-
-		return convertToIntArray(arr);
-	}
-
-	protected static int[] sortArray(int[] arr)
-	{
-		if(arr == null)
-			return null;
-
-		int[] sortedArray = Arrays.copyOf(arr, arr.length);
-
-		Arrays.sort(sortedArray);
-
-		return sortedArray;
+		assertNotNull(arr);
+		assertNotNull(arr1);
+		assertEquals(arr.length, unsortedIntArray.length);
+		assertEquals(arr.length, arr1.length);
 	}
 
 	@Test
 	public void testInsertionSort()
 	{
-		int[] res = Arrays.copyOf(unsortedArray, unsortedArray.length);
-
-		assertArrayEquals(InsertionSort.insertionSort(res), sortedArray);
+		assertNull(insertionSort(null));
+		assertArrayEquals(insertionSort(new int[0]), new int[0]);
+		assertArrayEquals(insertionSort(arr), sortedIntArray);
 	}
 
+	@Test
+	public void testTemplateInsertionSort()
+	{
+		assertNull(insertionSort(null, null));
+		assertNull(insertionSort(null, cmp));
+		assertArrayEquals(insertionSort(new Integer[0], null), new Integer[0]);
+		assertArrayEquals(insertionSort(arr1, null), arr1);
+		assertArrayEquals(insertionSort(arr1, cmp), convertToIntegerArray(sortedIntArray));
+	}
 }
