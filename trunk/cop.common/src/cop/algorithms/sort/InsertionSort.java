0d01836faa69e1c6364c51bdc20efb40de350142
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-08-16 08:41:14 +0000

    Modify comments

--- trunk/cop.common/src/cop/algorithms/sort/InsertionSort.java
+++ trunk/cop.common/src/cop/algorithms/sort/InsertionSort.java
@@ -20,7 +20,20 @@ import cop.common.predicates.MoreOrEqual;
  * entry at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort,
  * or merge sort.
  * <p>
- * However, insertion sort provides several advantages:
+ * <b>Algorithm:</b><br>
+ * Every repetition of insertion sort removes an element from the input data, inserting it into the correct position in
+ * the already-sorted list, until no input elements remain. The choice of which element to remove from the input is
+ * arbitrary, and can be made using almost any choice algorithm.
+ * <p>
+ * <b>Perfomance & Space:</b><br>
+ * <ul>
+ * <li>Worst case: O(n<sup>2</sup>)
+ * <li>Best case: O(n<sup>2</sup>)
+ * <li>Average case: O(n<sup>2</sup>)
+ * <li>Worst case space complexity: O(n) total, O(1) auziliary
+ * </ul>
+ * <p>
+ * <b>Advantages:</b><br>
  * <ul>
  * <li>Simple implementation
  * <li>Efficient for (quite) small data sets
@@ -30,7 +43,7 @@ import cop.common.predicates.MoreOrEqual;
  * sort or bubble sort; the best case (nearly sorted input) is O(n)
  * <li>Stable, i.e. does not change the relative order of elements with equal keys
  * <li>In-place, i.e. only requires a constant amount O(1) of additional memory space
- * <li>Online, i.e. can sort a list as it receives it
+ * <li>Online, i.e. can sort a list as it receives it.
  * </ul>
  * 
  * @author <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
