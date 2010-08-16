86d64f03845df4bccf055e7e2565be0e79749e3a
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-08-16 11:04:43 +0000

    Modify comments

--- trunk/cop.common/src/cop/algorithms/sort/SelectionSort.java
+++ trunk/cop.common/src/cop/algorithms/sort/SelectionSort.java
@@ -16,23 +16,18 @@ import cop.common.predicates.Less;
 import cop.common.predicates.NotEqual;
 
 /**
- * Selection sort is a sorting algorithm, specifically an in-place comparison sort. It has O(n2) complexity, making it
- * inefficient on large lists, and generally performs worse than the similar insertion sort. Selection sort is noted for
- * its simplicity, and also has performance advantages over more complicated algorithms in certain situations.
- * <p>
- * <b>Algorithm:</b><br>
- * The algorithm works as follows: find the minimum value in the list, swap it with the value in the first position,
- * repeat the steps above for the remainder of the list (starting at the second position and advancing each time).<br>
- * Effectively, the list is divided into two parts: the sublist of items already sorted, which is built up from left to
- * right and is found at the beginning, and the sublist of items remaining to be sorted, occupying the remainder of the
- * array.
+ * Bubble sort is a simple sorting algorithm. It works by repeatedly stepping through the list to be sorted, comparing
+ * each pair of adjacent items and swapping them if they are in the wrong order. The pass through the list is repeated
+ * until no swaps are needed, which indicates that the list is sorted. The algorithm gets its name from the way smaller
+ * elements "bubble" to the top of the list. Because it only uses comparisons to operate on elements, it is a comparison
+ * sort.
  * <p>
  * <b>Perfomance & Space:</b><br>
  * <ul>
  * <li>Worst case: O(n<sup>2</sup>)
- * <li>Best case: O(n<sup>2</sup>)
+ * <li>Best case: O(n)
  * <li>Average case: O(n<sup>2</sup>)
- * <li>Worst case space complexity: O(n) total, O(1) auxiliary
+ * <li>Worst case space complexity: O(1) auxiliary
  * </ul>
  * 
  * @author <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
