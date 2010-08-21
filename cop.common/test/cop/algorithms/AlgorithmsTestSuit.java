8d41425b6b25427f45b6cd1cfc4d8e6deffd4e46
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-08-21 06:20:05 +0000

    Add AlgorithmsTestSuit

---
+++ cop.common/test/cop/algorithms/AlgorithmsTestSuit.java
@@ -0,0 +1,11 @@
+package cop.algorithms;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import cop.algorithms.sort.SortTestSuit;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { SortTestSuit.class })
+public class AlgorithmsTestSuit
+{}
