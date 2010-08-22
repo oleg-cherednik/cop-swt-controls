30e2947d7a2aeeb8f8b517cc20f25825710883fe
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-08-22 23:22:24 +0000

    Add test suits

---
+++ cop.common/test/cop/common/CommonTestSuit.java
@@ -0,0 +1,11 @@
+package cop.common;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import cop.common.beans.JavaBeanTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { JavaBeanTest.class })
+public class CommonTestSuit
+{}
