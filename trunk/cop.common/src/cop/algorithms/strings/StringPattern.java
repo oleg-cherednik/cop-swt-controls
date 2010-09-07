b3fe384628a01a006a471606bdca021fb5fe071a
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-09-07 21:22:26 +0000

    Add StringPattern class

---
+++ trunk/cop.common/src/cop/algorithms/strings/StringPattern.java
@@ -0,0 +1,49 @@
+/**
+ * <b>License</b>: <a href="http://www.gnu.org/licenses/lgpl.html">GNU Leser General Public License</a>
+ * <b>Copyright</b>: <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
+ * 
+ * $Id$
+ * $HeadURL$
+ */
+package cop.algorithms.strings;
+
+public final class StringPattern
+{
+	/**
+	 * Replace giving String with specific pattern
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String quoteCharacters(final String str)
+	{
+		if(str == null || str.isEmpty())
+			return str;
+
+		final String template = "\"\r\n\\\'";
+		final char[][] prefix = { { '\\', '\"' }, { '\\', 'r' }, { '\\', 'n' }, { '\\', '\\' }, { '\\', '\'' } };
+		int total = 0;
+		int size = str.length();
+
+		for(int i = 0; i < size; i++)
+			if(template.indexOf(str.charAt(i)) >= 0)
+				total++;
+
+		char[] buf = new char[total * 2 + size - total];
+		char ch;
+
+		for(int i = 0, j = 0, k = 0; i < size; i++)
+		{
+			ch = str.charAt(i);
+			k = template.indexOf(ch);
+
+			if(k >= 0)
+				for(int kk = 0; kk < prefix[k].length; kk++)
+					buf[j++] = prefix[k][kk];
+			else
+				buf[j++] = ch;
+		}
+
+		return String.copyValueOf(buf);
+	}
+}
