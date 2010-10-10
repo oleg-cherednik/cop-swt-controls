a3f53fd77f471216deb8a8bd16dd453e30adb46a
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-10-10 21:40:20 +0000

    Add MatrixExtension with test

--- trunk/cop.common/test/cop/common/CommonTestSuit.java
+++ trunk/cop.common/test/cop/common/CommonTestSuit.java
@@ -4,8 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import cop.common.beans.JavaBeanTest;
+import cop.common.extensions.ExtensionsTestSuit;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { JavaBeanTest.class })
+@Suite.SuiteClasses({ JavaBeanTest.class, ExtensionsTestSuit.class })
 public class CommonTestSuit
 {}
