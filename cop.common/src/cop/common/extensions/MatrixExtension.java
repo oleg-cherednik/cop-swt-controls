4a23d69cd6bc29acc3320b41ef6d66225a6741f2
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-10-10 21:46:51 +0000

    Add MatrixExtensions javadox

--- cop.common/src/cop/common/extensions/MatrixExtension.java
+++ cop.common/src/cop/common/extensions/MatrixExtension.java
@@ -1,10 +1,26 @@
+/**
+ * <b>License</b>: <a href="http://www.gnu.org/licenses/lgpl.html">GNU Leser General Public License</a>
+ * <b>Copyright</b>: <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
+ * 
+ * $Id$
+ * $HeadURL$
+ */
 package cop.common.extensions;
 
+/**
+ * @author <a href="mailto:abba-bestl@mail.ru">Cherednik, Oleg</a>
+ */
 public final class MatrixExtension
 {
 	private MatrixExtension()
 	{}
 
+	/**
+	 * Create transpose matrix of giving one
+	 * 
+	 * @param matrix matrix
+	 * @return new matrix that is transpose to giving <b>matrix</b>
+	 */
 	public static int[][] transposeMatrix(int[][] matrix)
 	{
 		if(matrix == null)
