a3daef732d8639b4abaa2035325400ef53d1917f
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-11-27 09:55:26 +0000

    javadoc

--- trunk/cop.swt/src/cop/swt/widgets/viewers/ContentProviderAdapter.java
+++ trunk/cop.swt/src/cop/swt/widgets/viewers/ContentProviderAdapter.java
@@ -1,43 +1,68 @@
+/**
+ * @licence GNU Leser General Public License
+ *
+ * $Id$
+ * $HeadURL$
+ */
 package cop.swt.widgets.viewers;
 
 import java.util.Collection;
 
-import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
 import cop.swt.widgets.viewers.ibm._IStructuredContentProvider;
 
+/**
+ * Adapter class between {@link IStructuredContentProvider} that is used by SWT and {@link _IStructuredContentProvider}
+ * that is template version of the the {@link IStructuredContentProvider}.
+ * 
+ * @author <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
+ * @since 16.08.2010
+ */
 public final class ContentProviderAdapter<T> implements IStructuredContentProvider
 {
 	private final _IStructuredContentProvider<T> contentProvider;
 
+	/**
+	 * Creates new adapter instance. If
+	 * 
+	 * @param contentProvider content provider with template
+	 */
 	public ContentProviderAdapter(_IStructuredContentProvider<T> contentProvider)
 	{
-		Assert.isNotNull(contentProvider);
+		if(contentProvider == null)
+			throw new NullPointerException("Content provider can't be null");
 
 		this.contentProvider = contentProvider;
 	}
 
+	/**
+	 * @see IStructuredContentProvider#getElements(Object)
+	 */
 	@Override
 	@SuppressWarnings("unchecked")
 	public Object[] getElements(Object inputElement)
 	{
-		Assert.isNotNull(contentProvider);
-
 		return contentProvider.getElements((Collection<T>)inputElement).toArray();
 	}
 
+	/**
+	 * @see IStructuredContentProvider#dispose()
+	 */
 	@Override
 	public void dispose()
-	{}
+	{
+		contentProvider.dispose();
+	}
 
+	/**
+	 * @see IStructuredContentProvider#inputChanged(Viewer, Object, Object)
+	 */
 	@Override
 	@SuppressWarnings("unchecked")
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput)
 	{
-		Assert.isNotNull(contentProvider);
-
 		contentProvider.inputChanged(viewer, (Collection<T>)oldInput, (Collection<T>)newInput);
 	}
 }
