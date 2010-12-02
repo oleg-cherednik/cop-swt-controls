fbaadcb0cfdb0a051674d8f5cb9a59fe845ce0e9
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-12-02 19:13:42 +0000

    Remove warnings

--- trunk/cop.common/src/cop/common/extensions/DimentionExtension.java
+++ trunk/cop.common/src/cop/common/extensions/DimentionExtension.java
@@ -1,12 +1,8 @@
 package cop.common.extensions;
 
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.GC;
-
 public final class DimentionExtension
 {
 	private DimentionExtension()
 	{}
-
 
 }
