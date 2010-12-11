f52bd557bd2f73a018edb0e73b62b6a7b3fad7d7
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-12-11 09:37:13 +0000

    Rename 'menus' package to 'menu'

--- cop.swt/src/cop/swt/widgets/menus/interfaces/PropertyProvider.java
+++
@@ -1,7 +0,0 @@
-package cop.swt.widgets.menus.interfaces;
-
-public interface PropertyProvider<T>
-{
-	// must not return null
-	T getProperty();
-}
--- cop.swt/src/cop/swt/widgets/menus/interfaces/SortDirectionProvider.java
+++
@@ -1,8 +0,0 @@
-package cop.swt.widgets.menus.interfaces;
-
-import cop.swt.widgets.enums.SortDirectionEnum;
-
-public interface SortDirectionProvider
-{
-	SortDirectionEnum getSortDirection();
-}
