d4ff792226a1e579fdf69721b2b21e8001a456d8
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2010-12-11 09:37:13 +0000

    Rename 'menus' package to 'menu'

--- trunk/cop.swt/src/cop/swt/widgets/menus/interfaces/ICreateMenu.java
+++
@@ -1,9 +0,0 @@
-package cop.swt.widgets.menus.interfaces;
-
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
-
-public interface ICreateMenu
-{
-	MenuItem create(Menu parent);
-}
