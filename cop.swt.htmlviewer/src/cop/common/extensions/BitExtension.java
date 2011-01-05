2197d76664e5cd86240948595bbf71ba2001836b
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2011-01-05 08:08:01 +0000

    Remove dependence from 'cop.common' project

---
+++ cop.swt.htmlviewer/src/cop/common/extensions/BitExtension.java
@@ -0,0 +1,29 @@
+/**
+ * <b>License</b>: <a href="http://www.gnu.org/licenses/lgpl.html">GNU Leser General Public License</a>
+ * <b>Copyright</b>: <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
+ * 
+ * $Id$
+ * $HeadURL$
+ */
+package cop.common.extensions;
+
+/**
+ * Class provides different methods to work with bits.
+ * 
+ * @author <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
+ * @since 16.08.2010
+ */
+public final class BitExtension
+{
+	/**
+	 * Clear selected bit(s) in giving value
+	 * 
+	 * @param value value
+	 * @param bit bit or bit set to clear in the value
+	 * @return <code>value</code> with cleared selected bits
+	 */
+	public static int clearBits(int value, int bits)
+	{
+		return value & ~bits;
+	}
+}
