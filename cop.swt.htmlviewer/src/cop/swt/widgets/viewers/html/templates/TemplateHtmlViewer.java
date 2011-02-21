cd4ff7af8b610a78bcaae411981d75c0b880923b
oleg.cherednik <oleg.cherednik@80ba00be-f2ec-a629-1d6b-37def5fa00fe>
2011-02-21 05:11:14 +0000

    Simple modify interfaces

--- cop.swt.htmlviewer/src/cop/swt/widgets/viewers/html/templates/TemplateHtmlViewer.java
+++ cop.swt.htmlviewer/src/cop/swt/widgets/viewers/html/templates/TemplateHtmlViewer.java
@@ -2,8 +2,8 @@
  * <b>License</b>: <a href="http://www.gnu.org/licenses/lgpl.html">GNU Leser General Public License</a>
  * <b>Copyright</b>: <a href="mailto:abba-best@mail.ru">Cherednik, Oleg</a>
  * 
- * $Id:$
- * $HeadURL:$
+ * $Id$
+ * $HeadURL$
  */
 package cop.swt.widgets.viewers.html.templates;
 
@@ -19,9 +19,9 @@ import cop.swt.widgets.viewers.html.HtmlViewer;
  */
 public class TemplateHtmlViewer<T> extends HtmlViewer<T>
 {
-	private final HtmlTemplate<T> template;
+	private final IHtmlTemplate<T> template;
 
-	public TemplateHtmlViewer(Composite parent, int style, HtmlTemplate<T> template)
+	public TemplateHtmlViewer(Composite parent, int style, IHtmlTemplate<T> template)
 	{
 		super(parent, style, template.getHtmlDocument());
 
